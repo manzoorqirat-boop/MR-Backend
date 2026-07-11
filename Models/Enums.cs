919c6cd5046e16d43f634a363775161206d8e9a2
manzoorqirat-boop <manzoorqirat@gmail.com>
2026-07-11 16:19:23 +0530

    Update Enums.cs

--- Models/Enums.cs
+++ Models/Enums.cs
@@ -38,4 +38,22 @@ namespace SiteReportApp.Models
         Submitted = 1,
         Locked = 2
     }
+
+    // Who is logged in: a single-site user or the corporate/head-office team.
+    public enum UserRole
+    {
+        SiteUser = 0,
+        Corporate = 1
+    }
+
+    // Lifecycle of one site's monthly submission to corporate.
+    //  NotStarted -> (site enters data) -> Submitted -> Approved
+    //                                          \-> Returned (with comments) -> Submitted ...
+    public enum SubmissionStatus
+    {
+        NotStarted = 0,
+        Submitted = 1,
+        Approved = 2,
+        Returned = 3
+    }
 }
