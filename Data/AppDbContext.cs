696507da21a80e92247508b03b77a0b819a604ce
manzoorqirat-boop <manzoorqirat@gmail.com>
2026-07-13 17:27:20 +0530

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,44 +10,11 @@ namespace SiteReportApp.Data
         public DbSet<Site> Sites => Set<Site>();
         public DbSet<ReportPeriod> ReportPeriods => Set<ReportPeriod>();
         public DbSet<SiteSubmission> SiteSubmissions => Set<SiteSubmission>();
-        public DbSet<Initiative> Initiatives => Set<Initiative>();
-        public DbSet<TrainingRecord> TrainingRecords => Set<TrainingRecord>();
-        public DbSet<CostSavingInitiative> CostSavingInitiatives => Set<CostSavingInitiative>();
         public DbSet<ScorecardEntry> ScorecardEntries => Set<ScorecardEntry>();
         public DbSet<User> Users => Set<User>();
-        public DbSet<InitiativeAttachment> InitiativeAttachments => Set<InitiativeAttachment>();
-        public DbSet<InitiativeChangeRequest> InitiativeChangeRequests => Set<InitiativeChangeRequest>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // ---- InitiativeChangeRequest ----
-            modelBuilder.Entity<InitiativeChangeRequest>()
-                .HasOne(cr => cr.Initiative)
-                .WithMany()
-                .HasForeignKey(cr => cr.InitiativeId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<InitiativeChangeRequest>()
-                .Property(cr => cr.Status)
-                .HasConversion<string>();
-
-            modelBuilder.Entity<InitiativeChangeRequest>()
-                .Property(cr => cr.RequestType)
-                .HasConversion<string>();
-
-            modelBuilder.Entity<InitiativeChangeRequest>()
-                .HasIndex(cr => new { cr.InitiativeId, cr.Status });
-
-            // ---- InitiativeAttachment ----
-            modelBuilder.Entity<InitiativeAttachment>()
-                .HasOne(a => a.Initiative)
-                .WithMany()
-                .HasForeignKey(a => a.InitiativeId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<InitiativeAttachment>()
-                .HasIndex(a => a.InitiativeId);
-
             // ---- User ----
             modelBuilder.Entity<User>()
                 .HasIndex(u => u.Username)
@@ -92,90 +59,6 @@ namespace SiteReportApp.Data
                 .WithMany()
                 .HasForeignKey(ss => ss.ReportPeriodId)
                 .OnDelete(DeleteBehavior.Restrict);
-
-            // ---- Initiative ----
-            // Composite index: this is the shape every analytics query filters by
-            modelBuilder.Entity<Initiative>()
-                .HasIndex(i => new { i.ReportPeriodId, i.SiteId, i.Type });
-
-            // Natural key: one row per serial within a site/period/type. Prevents
-            // duplicate rows from a re-import or a double-submit (see DataEntryService upsert).
-            modelBuilder.Entity<Initiative>()
-                .HasIndex(i => new { i.SiteId, i.ReportPeriodId, i.Type, i.SerialNo })
-                .IsUnique();
-
-            modelBuilder.Entity<Initiative>()
-                .HasOne(i => i.Site)
-                .WithMany(s => s.Initiatives)
-                .HasForeignKey(i => i.SiteId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Initiative>()
-                .HasOne(i => i.ReportPeriod)
-                .WithMany(rp => rp.Initiatives)
-                .HasForeignKey(i => i.ReportPeriodId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Initiative>()
-                .Property(i => i.Type)
-                .HasConversion<string>();
-
-            modelBuilder.Entity<Initiative>()
-                .Property(i => i.Status)
-                .HasConversion<string>();
-
-            // ---- TrainingRecord ----
-            modelBuilder.Entity<TrainingRecord>()
-                .HasIndex(t => new { t.ReportPeriodId, t.SiteId });
-
-            modelBuilder.Entity<TrainingRecord>()
-                .HasIndex(t => new { t.SiteId, t.ReportPeriodId, t.SerialNo })
-                .IsUnique();
-
-            modelBuilder.Entity<TrainingRecord>()
-                .HasOne(t => t.Site)
-                .WithMany(s => s.TrainingRecords)
-                .HasForeignKey(t => t.SiteId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<TrainingRecord>()
-                .HasOne(t => t.ReportPeriod)
-                .WithMany(rp => rp.TrainingRecords)
-                .HasForeignKey(t => t.ReportPeriodId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<TrainingRecord>()
-                .Property(t => t.Status)
-                .HasConversion<string>();
-
-            // ---- CostSavingInitiative ----
-            modelBuilder.Entity<CostSavingInitiative>()
-                .HasIndex(c => new { c.ReportPeriodId, c.SiteId });
-
-            modelBuilder.Entity<CostSavingInitiative>()
-                .HasIndex(c => new { c.SiteId, c.ReportPeriodId, c.SerialNo })
-                .IsUnique();
-
-            modelBuilder.Entity<CostSavingInitiative>()
-                .HasOne(c => c.Site)
-                .WithMany(s => s.CostSavings)
-                .HasForeignKey(c => c.SiteId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<CostSavingInitiative>()
-                .HasOne(c => c.ReportPeriod)
-                .WithMany(rp => rp.CostSavings)
-                .HasForeignKey(c => c.ReportPeriodId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<CostSavingInitiative>()
-                .Property(c => c.ProjectStatus)
-                .HasConversion<string>();
-
-            // Decimal precision for money column (avoid Npgsql warning/truncation)
-            modelBuilder.Entity<CostSavingInitiative>()
-                .Property(c => c.PotentialSavingLacs)
-                .HasPrecision(14, 2);
 
             // ---- ScorecardEntry (Monthly Site Scorecard, 20-sheet metrics) ----
             // One row per (site, period, metric, rowIndex). The composite index is the
