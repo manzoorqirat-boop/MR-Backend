664b13f570cdeea7184a9ad37b304aaa4f1aa71d
manzoorqirat-boop <manzoorqirat@gmail.com>
2026-07-15 10:23:57 +0530

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -12,9 +12,58 @@ namespace SiteReportApp.Data
         public DbSet<SiteSubmission> SiteSubmissions => Set<SiteSubmission>();
         public DbSet<ScorecardEntry> ScorecardEntries => Set<ScorecardEntry>();
         public DbSet<User> Users => Set<User>();
+        public DbSet<Equipment> Equipments => Set<Equipment>();
+        public DbSet<AppSetting> AppSettings => Set<AppSetting>();
+        public DbSet<NotificationLog> NotificationLogs => Set<NotificationLog>();
+        public DbSet<MasterListItem> MasterListItems => Set<MasterListItem>();
+        public DbSet<QaItRegister> QaItRegisters => Set<QaItRegister>();
+        public DbSet<QaItPeriodicReview> QaItPeriodicReviews => Set<QaItPeriodicReview>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // ---- Settings & notifications ----
+            modelBuilder.Entity<AppSetting>()
+                .HasIndex(a => a.Key)
+                .IsUnique();
+
+            modelBuilder.Entity<NotificationLog>()
+                .HasIndex(n => n.DedupKey);
+
+            // ---- Master data ----
+            modelBuilder.Entity<Equipment>()
+                .HasIndex(e => new { e.SiteId, e.Code })
+                .IsUnique();
+
+            modelBuilder.Entity<Equipment>()
+                .HasOne(e => e.Site)
+                .WithMany()
+                .HasForeignKey(e => e.SiteId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<MasterListItem>()
+                .HasIndex(i => new { i.ListKey, i.Value })
+                .IsUnique();
+
+            // ---- QA-IT Compliance ----
+            modelBuilder.Entity<QaItRegister>()
+                .HasIndex(r => new { r.SiteId, r.Year })
+                .IsUnique();
+
+            modelBuilder.Entity<QaItRegister>()
+                .HasOne(r => r.Site)
+                .WithMany()
+                .HasForeignKey(r => r.SiteId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<QaItPeriodicReview>()
+                .HasIndex(r => new { r.SiteId, r.Year });
+
+            modelBuilder.Entity<QaItPeriodicReview>()
+                .HasOne(r => r.Site)
+                .WithMany()
+                .HasForeignKey(r => r.SiteId)
+                .OnDelete(DeleteBehavior.Cascade);
+
             // ---- User ----
             modelBuilder.Entity<User>()
                 .HasIndex(u => u.Username)
