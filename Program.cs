d66722dc916f7a79fe5f4431b6a39750508095af
manzoorqirat-boop <manzoorqirat@gmail.com>
2026-07-16 17:13:19 +0530

    Add QaItExcelService to service collection

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddScoped<ScorecardExcelService>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<AnalyticsService>();
 builder.Services.AddScoped<MasterDataExcelService>();
+builder.Services.AddScoped<QaItExcelService>();
 builder.Services.AddScoped<EmailService>();
 // The reminder scheduler is a singleton hosted service; the Admin page's
 // "run now" button (ReminderSettingsController) reuses the same instance.
